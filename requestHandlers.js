0bd6324d992fab69f0a27e14ff45b1895107ea8c
lidebiao1969 <362886827@qq.com>
2020-10-09 18:26:34 +0800

    --- Auto-Git Commit ---

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,28 +2,28 @@ var querystring = require("querystring"),
   fs = require("fs"),
   formidable = require("formidable");
 var picname;
-function start(response) {
-  console.log("Request handler 'start' was called.");
+// function start(response) {
+//   console.log("Request handler 'start' was called.");
 
-  var body =
-    "<html>" +
-    "<head>" +
-    '<meta http-equiv="Content-Type" ' +
-    'content="text/html; charset=UTF-8" />' +
-    "</head>" +
-    "<body>" +
-    '<form action="/upload" enctype="multipart/form-data" ' +
-    'method="post">' +
-    '<input type="file" name="upload" multiple="multiple">' +
-    '<input type="submit" value="Upload file" />' +
-    "</form>" +
-    "</body>" +
-    "</html>";
+//   var body =
+//     "<html>" +
+//     "<head>" +
+//     '<meta http-equiv="Content-Type" ' +
+//     'content="text/html; charset=UTF-8" />' +
+//     "</head>" +
+//     "<body>" +
+//     '<form action="/upload" enctype="multipart/form-data" ' +
+//     'method="post">' +
+//     '<input type="file" name="upload" multiple="multiple">' +
+//     '<input type="submit" value="Upload file" />' +
+//     "</form>" +
+//     "</body>" +
+//     "</html>";
 
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(body);
-  response.end();
-}
+//   response.writeHead(200, { "Content-Type": "text/html" });
+//   response.write(body);
+//   response.end();
+// }
 
 function upload(response, request) {
   console.log("upload called");
